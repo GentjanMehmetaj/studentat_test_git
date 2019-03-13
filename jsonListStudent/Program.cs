507fe075c996827e4fd2978eb6abd5a85fd8ae77
Gentjan Mehmetaj <gent.mehmetaj@gmail.com>
2019-03-14 00:41:11 +0100

    add program to the git

---
+++ jsonListStudent/Program.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonListStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                      
+          // string jsonsave1 = JsonConvert.SerializeObject(st1,Formatting.Indented);
+          // File.WriteAllText(@"C:\Users\albana\Desktop\Genti\json\jsonsave.json", JsonConvert.SerializeObject(st2, Formatting.Indented));
+          // string jsonsave2 = JsonConvert.SerializeObject(st2,Formatting.Indented);
+
+          //  Console.WriteLine(jsonsave1);
+          //  Console.WriteLine("\n");
+          //  Console.WriteLine(jsonsave2);
+            //Dictionary<string, int> piket = new Dictionary<string, int>
+            //{
+            //    {"Albana",100 },
+            //    {"Gentjan",85 }
+
+            //};
+            //string jsonpiket = JsonConvert.SerializeObject(piket, Formatting.Indented);
+            //Console.WriteLine(jsonpiket);
+            List<student> studentat = new List<student>
+            {
+                new student
+                { fName = "Albert",
+                    lName = "Ainstein",
+                    age = 30,
+                    TeDhenaPergjithesuese = new List<TePergjithshme>
+                    {
+                        new TePergjithshme()
+                        {
+                            email ="ainstainAlbert@gmail.com",
+                            country ="Germany",
+                            telNumber="00000000000"
+                        }
+
+                    }
+                },
+                new student
+                { fName = "Albert",
+                    lName = "Kamy",
+                    age = 60,
+                    TeDhenaPergjithesuese = new List<TePergjithshme>
+                    {
+                        new TePergjithshme()
+                        {
+                            email ="KamyAlbert@gmail.com",
+                            country ="United State",
+                            telNumber="111111111"
+                        }
+
+                    }
+                },
+                new student
+                { fName = "Nikollai",
+                    lName = "Gogol",
+                    age = 54,
+                    TeDhenaPergjithesuese = new List<TePergjithshme>
+                    {
+                        new TePergjithshme()
+                        {
+                            email ="NikollaiGogol@gmail.com",
+                            country ="Rusia",
+                            telNumber="222222222"
+                        }
+
+                    }
+                },
+                new student
+                {
+                fName = "Albana",
+                lName = "Sufaj",
+                age = 36,
+                TeDhenaPergjithesuese = new List<TePergjithshme>
+                {
+                    new TePergjithshme()
+                    {
+                        email ="albana.sufaj@gmail.com",
+                        country ="Albania",
+                        telNumber="068 80 87 218"
+                    }
+
+                }
+                },
+                new student
+                {
+                      fName = "Gentjan",
+                lName = "Mehmetaj",
+                age = 36,
+                TeDhenaPergjithesuese = new List<TePergjithshme>
+                {
+                    new TePergjithshme()
+                    {
+                        email ="gent.mehmetaj@gmail.com",
+                        country ="Albania",
+                        telNumber="069 40 49 178"
+                    }
+
+                }
+                }
+            };
+            File.WriteAllText(@"C:\Users\albana\Desktop\Genti\json\studentat.json", JsonConvert.SerializeObject(studentat, Formatting.Indented));
+            string listST = JsonConvert.SerializeObject(studentat, Formatting.Indented);
+            List<student> listStudetash = JsonConvert.DeserializeObject<List<student>>(listST);
+            foreach (var st in listStudetash)
+            {
+                Console.WriteLine(st.fName);
+                Console.WriteLine(st.lName);
+                Console.WriteLine(st.age);
+                foreach (var el in st.TeDhenaPergjithesuese)
+                {
+                    Console.WriteLine(el.email);
+                    Console.WriteLine(el.country);
+                    Console.WriteLine(el.telNumber);
+                    Console.WriteLine("\n");
+                }
+            }
+            Console.ReadLine();
+               
+                
+        }
+    }
+}
---
+++ jsonListStudent/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonListStudent
+{
+    class student
+    { 
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public int age { get; set; }
+        public List<TePergjithshme> TeDhenaPergjithesuese { get; set; }
+    }
+}
